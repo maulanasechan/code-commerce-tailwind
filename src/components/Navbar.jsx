b64159f70f501b0e87c7526614835420c3500511
Maulana Sechan <maulanasechan123@gmail.com>
2022-08-17 00:36:28 +0700

    menambahkan hero atau jumbotron

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,31 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
 
   return (
-    <div className="flex justify-between items-center h-24 mx-auto max-w-[1240px] px-4 text-white">
-      <h1 className="text-3xl font-bold text-[#00df9a] w-full">React.</h1>
+    <div className="flex justify-between items-center h-24 mx-auto md:max-w-[1240px] px-4 text-white max-w-[450px]">
+      <h1 className="text-3xl font-bold text-[#00df9a] w-full cursor-pointer">
+        React.
+      </h1>
       <ul className="md:flex hidden">
-        <li className="p-4">Home</li>
-        <li className="p-4">Company</li>
-        <li className="p-4">Resources</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Contact</li>
+        <li className="p-4 cursor-pointer hover:text-gray-500 font-semibold">
+          Home
+        </li>
+        <li className="p-4 cursor-pointer hover:text-gray-500 font-semibold">
+          Company
+        </li>
+        <li className="p-4 cursor-pointer hover:text-gray-500 font-semibold">
+          Resources
+        </li>
+        <li className="p-4 cursor-pointer hover:text-gray-500 font-semibold">
+          About
+        </li>
+        <li className="p-4 cursor-pointer hover:text-gray-500 font-semibold">
+          Contact
+        </li>
       </ul>
-      <div onClick={() => setNav(!nav)} className="flex md:hidden">
+      <div
+        onClick={() => setNav(!nav)}
+        className="flex md:hidden cursor-pointer"
+      >
         {!nav ? (
           <RiCloseCircleLine
             size={20}
@@ -41,19 +56,27 @@ const Navbar = () => {
       <div
         className={
           !nav
-            ? "fixed top-0 w-[60%] h-full left-[-100%] ease-in-out duration-1000 delay-500"
-            : "fixed top-0 left-0 w-[60%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-1000 delay-500"
+            ? "fixed top-0 w-[70%] h-full left-[-100%] ease-in-out duration-1000 delay-500"
+            : "fixed top-0 left-0 w-[70%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-1000 delay-500"
         }
       >
-        <h1 className="text-3xl font-bold text-[#00df9a] m-4 mt-[30px]">
+        <h1 className="text-3xl font-bold text-[#00df9a] ml-10 mt-[30px] cursor-pointer">
           React.
         </h1>
-        <ul className="uppercase p-4">
-          <li className="p-4 border-b border-gray-600">Home</li>
-          <li className="p-4 border-b border-gray-600">Company</li>
-          <li className="p-4 border-b border-gray-600">Resources</li>
-          <li className="p-4 border-b border-gray-600">About</li>
-          <li className="p-4">Contact</li>
+        <ul className="uppercase p-12">
+          <li className="p-4 border-b border-gray-600 cursor-pointer hover:font-bold">
+            Home
+          </li>
+          <li className="p-4 border-b border-gray-600 cursor-pointer hover:font-bold">
+            Company
+          </li>
+          <li className="p-4 border-b border-gray-600 cursor-pointer hover:font-bold">
+            Resources
+          </li>
+          <li className="p-4 border-b border-gray-600 cursor-pointer hover:font-bold">
+            About
+          </li>
+          <li className="p-4 cursor-pointer hover:font-bold">Contact</li>
         </ul>
       </div>
     </div>
